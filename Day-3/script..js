3de51d4e35f76c905f07d0276f59b15bdac006a3
Krishna Singh <arushsingh8103@gmail.com>
2025-03-11 22:21:23 +0530

    DAY-3

---
+++ Day-3/script..js
@@ -0,0 +1,21 @@
+ let str = "Krishna Singh"
+
+//  var i =str.toLowerCase()
+//  console.log(i)
+//  var a = str.toUpperCase()
+//  console.log(a)
+//  var b = str.trim()
+//  console.log(b)
+//  var c = str.charAt(0)
+//  console.log(c)
+
+//  var d = str.replace("Krishna", "Krish")
+//  var e = str.slice(0,5)
+
+
+//  let fullName = prompt("Enter Yor Full NAme :")
+//  let userName = fullName + fullName.length
+//  console.log(userName)
+
+let ans = str.split('i')
+console.log(ans)
