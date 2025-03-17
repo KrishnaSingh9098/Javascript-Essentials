5d01f03450a0f7241b095b1277aa14bc44edb29d
Krishna Singh <arushsingh8103@gmail.com>
2025-03-17 22:18:23 +0530

    new

---
+++ Day-3/script.js
@@ -0,0 +1,66 @@
+ let str = "Krishna Singh"
+
+//  var i =str.toLowerCase()
+//  console.log(i)
+//  var a = str.toUpperCase()
+//  console.log(a)
+//  var b = str.trim()
+//  console.log(b)
+//  var c = str.charAt(0)
+//  console.log(c)
+
+//  var d = str.replace("Krishna", "Krish")
+//  var e = str.slice(0,5)
+
+
+//  let fullName = prompt("Enter Yor Full NAme :")
+//  let userName = fullName + fullName.length
+//  console.log(userName)
+
+// let ans = str.split('i')
+// console.log(ans)
+// console.log(str)
+
+//   let ans = str.includes(" ");
+
+//   console.log(ans);
+
+
+// 12 > 111  ? console.log("hey") : console.log("heheheh")
+
+
+// switch("Krishna"){
+//     case "Krish" : console.log("You Are Krish")
+//     break;
+//     case "Annu" : console.log("You Are Anupa")
+//     break;
+//     case "Akansha" : console.log("You Are Krisna's GirlFriend")
+//     break;
+//     default :
+//     console.log("You are not Krishna, Annu or Akansha")
+// }
+
+
+// let i=12;
+// do{
+//     console.log(i)
+// i++;
+// }
+
+// while(i< 15)
+
+
+// for(var i=0;i<=12;i++){
+//     console.log(i)
+//     if(i===8) break
+// }
+
+// for(var i=0;i<=12;i++){
+  
+//     if(i===8) continue;
+//     console.log(i)
+// }
+
+
+
+
---
+++ Day-4/script.js
@@ -0,0 +1,30 @@
+
+
+// function krishna(){
+//     console.log("Helo Babies")
+//     let str = "Krishna Singh"
+//     let ans = str.length
+//     console.log(ans)
+// }
+
+
+// krishna()
+// krishna()
+// krishna()
+// krishna()
+// krishna()
+
+// function can be called many times .
+
+
+
+// function ghrPerAAo (){
+//     console.log("BAbay")
+// }
+
+// ghrPerAAo()
+
+
+// let str = "Hello javaScript!";
+// console.log(str.includes("Java"))
+
