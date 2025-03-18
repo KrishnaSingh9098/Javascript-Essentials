90050b47e23e8c333639c73473e8235f5fa657f4
Krishna Singh <arushsingh8103@gmail.com>
2025-03-19 00:34:59 +0530

    new

--- Day-4/script.js
+++ Day-4/script.js
@@ -28,3 +28,82 @@
 // let str = "Hello javaScript!";
 // console.log(str.includes("Java"))
 
+
+// console.log("IMMEDIATELY WORKS")   /// THis code Works Immedialety in the console without calling the anythin.
+
+// function Krishna(){
+//     console.log("Hello Guys")
+// }
+
+// Krishna()
+// Krishna()
+// Krishna()
+
+// Functiopns areStart doing their work whenever trhey wee Called
+
+// #PAssinr a parameter in a Function.
+
+// function greet(name){
+//     console.log(name)
+// }
+
+// greet('hEllo world')
+// greet('hii')
+// greet('byy')
+// greet('Krisnna')
+// greet('Singh')
+
+
+
+
+// function ABCD(a,b,c,d){
+//              console.log(a,b,c,d)
+// }
+ 
+// ABCD(1,20,3,56)
+// ABCD()
+
+
+
+// function Shayri(a){
+//     console.log(a)
+    
+// }
+
+// Shayri('Tumse kisne Kaha Ki Daga Krta Hu Mai ; Agar Krne Per Aau To Sirf Vafa Krta Hu Mai.')
+// Shayri("Kya Vo Sab Baate Sunai Deti HAi Tujhe Jo Teri Tasveero Se Kara Krta Hu Mai.")
+
+
+
+
+// #  Fat Arrow Function With Implicit Return .  Apne App Return Krna Without Return KEyword.
+ //   #Or jab humm krte hai toh vo explixit Return hota hai
+
+// function abcd(){
+//     return 12;
+// }
+
+
+//   let cc = abcd()
+//   console.log(cc)
+
+
+
+// Rest Parameter 
+
+function abcd(a,b,c,...rest){
+    console.log(a,b,c,rest)
+}
+
+abcd(1,2,3,4,5,6,7,8,9)
+
+
+function logMessages(...messages) {
+    console.log(messages);  // Output will be an empty array
+  }
+  
+  logMessages("Hello");
+  logMessages();
+  
+
+ 
