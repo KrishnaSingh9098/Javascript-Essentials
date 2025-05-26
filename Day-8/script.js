758cbfdfa3054758ad7e38bf63565e434e7c34c1
Krishna Pratap Singh <153250693+KrishnaSingh9098@users.noreply.github.com>
2025-05-26 08:52:36 +0530

    BOM

--- Day-8/script.js
+++ Day-8/script.js
@@ -1,1 +1,49 @@
-console.log("Hello  Babies ")
+console.log(window)
+
+//                                                     DOM MANIPULATION
+
+var h1 = document.querySelector('h1')
+console.log(h1)
+
+//                                                      EVENT LISTENERS
+
+h1.addEventListener('click',function(){
+    console.log('hey guuys')
+    h1.innerHTML = "Vartamaan Ankho k dhoka hai"
+     h1.style.fontSize = "100px"
+h1.style.color = "green"
+h1.style.backgroundColor = "#45aff5"
+})
+
+
+
+
+// Select the h1 element by ID
+const heading = document.querySelector('#main-heading');
+heading.textContent = 'Hello from querySelector!';
+
+// Select the paragraph element by class
+const paragraph = document.querySelector('.description');
+paragraph.style.color = 'blue';
+
+
+
+// Select all elements with the class "item"
+const items = document.querySelectorAll('.item');
+
+// Loop through each item and change its text color
+// items.forEach((item, index) => {
+//   item.style.color = 'green';
+//   item.textContent = `Updated Item ${index + 1}`;
+// });
+
+items[0].innerHTML = "Helllo Sexy Lady"
+
+
+
+let b=document.querySelector("#box")
+console.log(b)
+// b.textContent="<h1>HELLO GUYS</>"
+ b.innerHTML = 'changed'
+
+
