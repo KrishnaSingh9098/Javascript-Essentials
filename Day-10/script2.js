d044f20fdd18f7d04fb9471e63cbe4b09db8c938
Krishna Pratap Singh <153250693+KrishnaSingh9098@users.noreply.github.com>
2025-06-05 20:15:40 +0530

    JJJ

--- Day-10/script2.js
+++ Day-10/script2.js
@@ -18,14 +18,56 @@ const card = document.querySelector('.card');
 //   console.log("Hello World");
 // };
 
+
 card.addEventListener('click',function (){
   console.log("CARD WAS CLICKED AND ITS MADE A NW CARD")
   
       const newCard = document.createElement("div");
       newCard.className = "card";
-      newCard.textContent = "New Card";
+      newCard.textContent = "CARD";
       document.body.append(newCard);
+      container.append(newCard)
 })
+
+
+
+// THis cod make a 50card on a single click 
+
+
+
+
+// card.addEventListener('click',function (){
+//   console.log("CARD WAS CLICKED AND ITS MADE A NW CARD")
+//   for(let i=1;i<=50;i++){
+    
+//       const newCard = document.createElement("div");
+//       newCard.className = "card";
+//       newCard.textContent = i;
+//     //   document.body.append(newCard);
+//       container.append(newCard)
+//   }
+// })
+
+
+
+// With this cod it wwill prints one div on a single click and with thtir counting :
+
+
+// let i=1;
+
+// card.addEventListener('click',function (){
+//   console.log("CARD WAS CLICKED AND ITS MADE A NW CARD")
+  
+//       const newCard = document.createElement("div");
+//       newCard.className = "card";
+//       newCard.textContent = `card${i}`;
+//       i++
+
+//       document.body.append(newCard);
+//       container.append(newCard)
+// })
+
+
 
 
 
