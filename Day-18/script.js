d8d380f5540c23dc271ac2e154773e67186e3823
Krishna Pratap Singh <153250693+KrishnaSingh9098@users.noreply.github.com>
2025-06-28 17:20:17 +0530

    GIT BY VISVA MOHAN

--- Day-18/script.js
+++ Day-18/script.js
@@ -1,21 +1,43 @@
 const myNameTag = document.querySelector('.name-tag');
+const ageTag = document.querySelector('.age-tag');
+
 const nameInput = document.querySelector('.name');
 const ageInput = document.querySelector('.age');
 
-// Load name and age from localStorage when page loads
-nameInput.value = localStorage.getItem('myName') || '';
-ageInput.value = localStorage.getItem('myAge') || '';
-myNameTag.innerHTML = nameInput.value;
+// // Load name and age from localStorage when page loads
+// nameInput.value = localStorage.getItem('myName') || '';
+// ageInput.value = localStorage.getItem('myAge') || '';
+// myNameTag.innerHTML = nameInput.value;
 
-// Save name on input
-nameInput.addEventListener('input', (e) => {
-  const name = e.target.value;
-  myNameTag.innerHTML = name;
-  localStorage.setItem('myName', name);
-});
+// // Save name on input
+// nameInput.addEventListener('input', (e) => {
+//   const name = e.target.value;
+//   myNameTag.innerHTML = name;
+//   localStorage.setItem('myName', name);
+// });
 
-// Save age on input
-ageInput.addEventListener('input', (e) => {
-  const age = e.target.value;
-  localStorage.setItem('myAge', age);
-});
+// // Save age on input
+// ageInput.addEventListener('input', (e) => {
+//   const age = e.target.value;
+//   localStorage.setItem('myAge', age);
+// });
+
+
+const myData = JSON.parse(localStorage.getItem('myData')|| {})
+
+nameInput.addEventListener('input', (e)=>{
+  myData.myName = e.target.value;
+  localStorage.setItem('myData', JSON.stringify(myData))
+  myNameTag.innerHTML = e.target.value;
+})
+
+
+ageInput.addEventListener('input', (e)=>{
+  myData.age = e.target.value;
+  localStorage.setItem('myData', JSON.stringify(myData))
+  ageTag.innerHTML = e.target.value
+})
+
+
+ageInput.innerHTML= myData.ageTag
+nameInput.innerHTML = myData.myName
