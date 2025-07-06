c50a44102bf1d0105beb5181a6567daca46e0ecc
Krishna Pratap Singh <153250693+KrishnaSingh9098@users.noreply.github.com>
2025-07-06 16:23:28 +0530

    heLLO

---
+++ Day-20/test.sh
@@ -0,0 +1,13 @@
+# num1=20;
+# num2=30;
+# echo $((num1+num2))
+# echo "END"
+
+
+
+for i in {1...50}; do touch "app$i.js"; 
+
+
+//To remove the files app1.js through app50.js, use this Bash command:
+
+// rm -f app{1..50}.js
